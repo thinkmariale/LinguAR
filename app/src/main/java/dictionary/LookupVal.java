0d89d12dc1ee6336d1fc729e0ed42c011fbf98c8
thundertoss <thundertoss@gmail.com>
2014-10-05 02:42:42 -0400

    Added dictionary package

---
+++ app/src/main/java/dictionary/LookupVal.java
@@ -0,0 +1,39 @@
+package dictionary;
+
+import java.util.HashSet;
+
+public class LookupVal {
+	public String word;
+	public String translation;
+	public WordStat stats;
+	public HashSet<String> categoryList;
+	
+	public LookupVal(){
+		word = "";
+		translation = "";
+		stats = new WordStat();
+		categoryList = new HashSet<String>(5);
+	}
+	
+	public LookupVal(String w){
+		word = w;
+		translation = "";
+		stats = new WordStat();
+		categoryList = new HashSet<String>(5);
+	}
+	
+	public LookupVal(String w, String t, int c, int i, HashSet<String> cats){
+		word = w;
+		translation = t;
+		stats = new WordStat(w,c,i);
+		categoryList = cats;		
+	}
+	
+	public LookupVal(String w, String t){
+		word = w;
+		translation = t;
+		stats = new WordStat(w);
+		categoryList = new HashSet<String>(5);
+	}
+	
+}
