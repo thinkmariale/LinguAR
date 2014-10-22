d611592d7e368e2f3e5e34df9b5cbddcc1ae1bef
Adarshkumar Pavani <adarshkumar.pavani@gmail.com>
2014-10-22 10:26:27 -0400

    Adding a framework for Lesson Plan

---
+++ LinguAR/src/com/linguar/lessonplan/ReviewMode.java
@@ -0,0 +1,37 @@
+package com.linguar.lessonplan;
+import dictionary.dictionary_populator;
+import java.util.*;
+import java.util.List;
+
+public class ReviewMode {
+	
+	//Call to saved categories
+	private ArrayList<HashMap<String,List<String>>> savedCategories;
+	
+	//initializer
+	{
+		savedCategories = new ArrayList<HashMap<String,List<String>>>();
+	}
+	
+	//Call to category dictionary
+	private HashMap<String, List<String>> getCategoryDic()
+	{
+		HashMap<String, List<String>>  catDictionary = null;
+		
+		return catDictionary;
+	}
+	
+	//Call to all the words in a category dictionary
+	
+	
+	//Shuffle the returned set of words
+	
+	
+	//Repeat them 3 times and update their repeat count and timestamp
+	
+	
+	//Start off a timer that would keep track of the words that have started.
+	
+	
+
+}
